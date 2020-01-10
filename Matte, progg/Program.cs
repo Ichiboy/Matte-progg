45c5bd30149ea0fb5281353432afc98fb178597b
max.mustonensjolund <max.mustonensjolund@elev.ga.ntig.se>
2020-01-10 10:38:34 +0100

    Fungerande K

--- Matte, progg/Program.cs
+++ Matte, progg/Program.cs
@@ -10,6 +10,44 @@ namespace Matte__progg
     {
         static void Main(string[] args)
         {
+            int y = 0;
+            int k = 0;
+            int x = 0;
+            int m = 0;
+            double punkt1y = 0;
+            double punkt1x = 0;
+            double punkt2y = 0;
+            double punkt2x = 0;
+
+            Console.WriteLine("Vad är X på punkt 1?");
+            punkt1x = double.Parse(Console.ReadLine());
+            
+            
+
+            Console.WriteLine("Vad är Y på punkt 1?");
+            punkt1y = double.Parse(Console.ReadLine());
+            
+            
+
+            Console.WriteLine("Vad är X på punkt 2?");
+            punkt2x = double.Parse(Console.ReadLine());
+            
+            
+
+            Console.WriteLine("Vad är Y på punkt 2?");
+            punkt2y = double.Parse(Console.ReadLine());
+            
+            
+
+            Console.WriteLine("Alltså är Punkt 1" + " (" + punkt1x + "," + punkt1y + ")");
+            Console.WriteLine("Alltså är Punkt 2" + " (" + punkt2x + "," + punkt2y + ")");
+            Console.ReadKey();
+
+            Console.WriteLine("Dax att räkna ut K!");
+            Console.WriteLine("K=" + (punkt2y - punkt1y) / (punkt2x - punkt1x));
+            Console.ReadKey();
+
+        
         }
     }
 }
